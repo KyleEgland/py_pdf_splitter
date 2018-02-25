cb6353ee1e05fdb8568755a20e6f1bdb1becca5e
MediocreMarine <kyle.egland@outlook.com>
2018-02-25 17:00:36 -0600

    Adjusted tab layout

--- single_split_tab.py
+++ single_split_tab.py
@@ -28,78 +28,101 @@ class SingleSplitTab(tk.Frame):
         tk.Grid.columnconfigure(self, 1, weight=1)
         tk.Grid.columnconfigure(self, 2, weight=1)
         tk.Grid.columnconfigure(self, 3, weight=1)
+        tk.Grid.columnconfigure(self, 4, weight=1)
+        tk.Grid.columnconfigure(self, 5, weight=1)
+        tk.Grid.columnconfigure(self, 6, weight=1)
+        tk.Grid.columnconfigure(self, 7, weight=1)
+        tk.Grid.columnconfigure(self, 8, weight=1)
 
-        #############################
-        # Row 0 - INPUT INFORMATION #
-        #############################
+        #######################
+        # Row 0 - INPUT LABEL #
+        #######################
         # A status
         # Input file label
         self.input_file_lbl = tk.Label(self, text='Select Source PDF File')
         # Input file label placement
-        self.input_file_lbl.grid(row=0, column=0, sticky='sne', pady=5)
+        self.input_file_lbl.grid(row=0, column=0, columnspan=4, sticky='snw',
+                                 padx=2, pady=2)
 
+        #########################
+        # Row 1 - INPUT WIDGETS #
+        #########################
         # Input file entry widget
         self.input_file_entry = tk.Entry(self)
         # Input entry field placement
-        self.input_file_entry.grid(row=0, column=1, columnspan=2)
+        self.input_file_entry.grid(row=1, column=0, columnspan=3, sticky='ew')
 
         # Input file selection button
-        self.input_file_btn = tk.Button(self, text='Select File')
+        self.input_file_btn = ttk.Button(self, text='Select File')
         # Input file selection button placement
-        self.input_file_btn.grid(row=0, column=3, sticky='nsw', padx=5, pady=5)
+        self.input_file_btn.grid(row=1, column=3, sticky='ew', padx=5, pady=5)
 
-        ############################
-        # Row 1 - OUTPUT DIRECTORY #
-        ############################
+        ########################
+        # Row 2 - OUTPUT LABEL #
+        ########################
         # Output directory label
         self.out_dir_lbl = ttk.Label(self, text='Output Directory')
         # Output directory label placement
-        self.out_dir_lbl.grid(row=1, column=0, sticky='nes', padx=5, pady=5)
+        self.out_dir_lbl.grid(row=2, column=0, columnspan=4, sticky='snw',
+                              padx=2, pady=2)
 
+        ##########################
+        # Row 3 - OUTPUT WIDGETS #
+        ##########################
         # Output directory entry field
         self.out_dir_entry = tk.Entry(self)
         # Output directory entry field placement
-        self.out_dir_entry.grid(row=1, column=1, columnspan=2)
+        self.out_dir_entry.grid(row=3, column=0, columnspan=3, sticky='ew')
 
         # Output directory selection button
-        self.out_btn = tk.Button(self, text='Select Directory')
+        self.out_btn = ttk.Button(self, text='Select Directory')
         # Output directory selection button placement
-        self.out_btn.grid(row=1, column=3, sticky='nsw', padx=5, pady=5)
+        self.out_btn.grid(row=3, column=3, sticky='ew', padx=5, pady=5)
 
-        ############################
-        # Row 2 = OUTPUT FILE NAME #
-        ############################
+        #############################
+        # Row 4 - OUTPUT FILE LABEL #
+        #############################
         # Output file name label
         self.outfile_name_lbl = ttk.Label(self, text='Output File Name')
         # Output file name label placement
-        self.outfile_name_lbl.grid(row=2, column=0, sticky='nes', padx=5,
-                                   pady=5)
+        self.outfile_name_lbl.grid(row=4, column=0, columnspan=4, sticky='snw',
+                                   padx=2, pady=2)
+
+        ##############################
+        # Row 5 - OUTPUT FILE WIDGET #
+        ##############################
         # Output file entry field
         self.outfile_name_entry = tk.Entry(self)
         # Output file entry feild placement
-        self.outfile_name_entry.grid(row=2, column=1, columnspan=3)
+        self.outfile_name_entry.grid(row=5, column=0, columnspan=4,
+                                     sticky='ew')
 
-        ######################
-        # Row 3 = PAGE RANGE #
-        ######################
+        ############################
+        # Row 6 - PAGE RANGE LABEL #
+        ############################
         # Page range label
         self.page_rng_lbl = ttk.Label(self, text='Page Range Selection (I.e. \
 1-2,5)')
         # Page range label placement
-        self.page_rng_lbl.grid(row=3, column=0, sticky='nes', padx=5, pady=5)
+        self.page_rng_lbl.grid(row=6, column=0, columnspan=4, sticky='snw',
+                               padx=2, pady=2)
+
+        ##############################
+        # Row 7 - PAGE RANGE WIDGETS #
+        ##############################
         # Page Range entry field
         self.page_rng_entry = tk.Entry(self)
         # Page Range entry field placement
-        self.page_rng_entry.grid(row=3, column=1, columnspan=3)
+        self.page_rng_entry.grid(row=7, column=0, columnspan=4, sticky='ew')
 
         ###########################
-        # Row 4 = PROCESS BUTTONS #
+        # Row 8 - PROCESS BUTTONS #
         ###########################
         # Process button
         self.process_btn = ttk.Button(self, text='Process')
         # Process button placement
-        self.process_btn.grid(row=4, column=1, sticky='nsw')
+        self.process_btn.grid(row=8, column=1, sticky='ew', padx=5, pady=5)
         # Quit button
         self.quit_btn = ttk.Button(self, text='Quit')
         # Quit button placement
-        self.quit_btn.grid(row=4, column=2, sticky='nes')
+        self.quit_btn.grid(row=8, column=2, sticky='ew', padx=5, pady=5)
